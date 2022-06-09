ca4d4f292fdaf63edc0c36dbf5793229876b7b72
AlaaAlTawil <Alaa.m.Tawil@outlook.com>
2022-06-09 21:27:16 +0300

    implementing jwt and some api's

---
+++ backend/Ecommercebackend/app/Http/Controllers/ItemController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Item;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller
+{
+    public function getitems(){
+        $items=Item::all();
+        return response()->json([
+            "status" => "success",
+            "items" => $items
+        ],200);
+
+    }
+    public function addItem(Request $request){
+        $item=new Item;
+        $item->Name=$request->itemname;
+        $item->Price=$request->itemprice;
+        $item->Categoryid=$request->cat_id;
+        $item->Img=$request->img;
+        $item->save();
+        return response()->json([
+            "status" => "success"],200);
+    }
+    
+}
---
+++ backend/Ecommercebackend/routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JWTController;
+use App\Http\Controllers\ItemController;
+
+Route::group(['middleware' => 'api'], function($router) {
+    Route::post('/register', [JWTController::class, 'register']);
+    Route::post('/login', [JWTController::class, 'login']);
+    Route::post('/logout', [JWTController::class, 'logout']);
+    Route::post('/refresh', [JWTController::class, 'refresh']);
+    Route::post('/profile', [JWTController::class, 'profile']);
+});
+
+Route::get('/getitems', [ItemController::class, 'GetItems']);
+Route::post('/additem', [ItemController::class, 'addItem']);
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
